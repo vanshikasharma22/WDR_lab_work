861eb8d0bbc2b1a99e9d89d9c5a7de6ec8cc3cde
Vanshika Sharma <vanshikabhardwaj82@gmail.com>
2025-11-12 12:15:44 +0530

    List By React

---
+++ List/App.jsx
@@ -0,0 +1,24 @@
+function List() {
+  return (
+    <div>
+      <h4> Ques 1</h4>
+      <ol>
+        <li>apple</li>
+        <li>mango</li>
+        <li>grape</li>
+        <li>kiwi</li>
+        <li>orange</li>
+      </ol>
+      <br />
+      <br />
+      <h4>Ques 2</h4>
+      <ul>
+        <li>Hindi</li>
+        <li>English</li>
+        <li>Maths</li>
+        <li>Computer</li>
+        <li>Science</li>
+      </ul>
+    </div>
+  );
+}
